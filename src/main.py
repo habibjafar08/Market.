8ee84470620f41aa2f56dfbb7d242faaaeee15a6
Habib Jafar Nuur <habibjafar08@gmail.com>
2024-02-09 14:03:52 +0700

    update

--- src/main.py
+++ src/main.py
@@ -2,20 +2,49 @@
 price_apel = 10000
 price_jeruk = 15000
 price_anggur = 20000
-# Meminta input jumlah apel kepada user
-    
-n_apel = int(input('Masukkan jumlah apel: '))
-n_jeruk = int(input('Masukkan jumlah jeruk: '))
-n_anggur = int(input("Masukkan jumlah apel: "))
-# Menghitung total belanjaan per buah
-total_apel = n_apel * price_apel
-total_jeruk = n_jeruk * price_jeruk
-total_anggur = n_anggur * price_anggur
-# Menghitung total belanjaan
-total = total_apel + total_jeruk + total_anggur
 
-# Show
-print(f'''Detail Belanjaan
+# Mendefinisikan stock buah
+stock_apel = 5
+stock_jeruk = 10
+stock_anggur = 7
+
+# test
+while True:
+
+    # Meminta input jumlah apel yang dibeli kepada user
+    while True:
+        n_apel = int(input('Masukkan jumlah apel: '))
+        if n_apel > stock_apel:
+            print(f'Jumlah pesanan tidak terpenuhi, stock Apel sisa {stock_apel}')
+        else:
+            break
+
+    # Meminta input jumlah jeruk yang dibeli kepada user
+    while True:
+        n_jeruk = int(input('Masukkan jumlah jeruk: '))
+        if n_jeruk > stock_jeruk:
+            print(f'Jumlah pesanan tidak terpenuhi, stock Jeruk sisa {stock_jeruk}')
+        else:
+            break
+
+    # Meminta input jumlah anggur yang dibeli kepada user
+    while True:
+        n_anggur = int(input('Masukkan jumlah anggur: '))
+        if n_anggur > stock_anggur:
+            print(f'Jumlah pesanan tidak terpenuhi, stock Anggur sisa {stock_anggur}')
+        else:
+            break
+
+    # Menghitung total belanjaan per buah
+    total_apel = n_apel * price_apel
+    total_jeruk = n_jeruk * price_jeruk
+    total_anggur = n_anggur * price_anggur
+
+    # Menghitung total belanjaan
+    total = total_apel + total_jeruk + total_anggur
+
+    # Show
+    print(f'''\nDetail Belanjaan
         
     Apel : {n_apel} x {price_apel} = {total_apel}
     Jeruk : {n_jeruk} x {price_jeruk} = {total_jeruk}
@@ -24,6 +53,23 @@ print(f'''Detail Belanjaan
     Total : {total}
     ''')
 
-    
+    # Melakukan pembayaran
+    while True:
+        bayar = int(input('Silahkan masukkan uang Anda:'))
+        if bayar >= total:
+            print('\nTerima kasih')
+            print(f'Uang kembalian Anda sebesar {bayar - total}\n')
+            break
+        else:
+            print(f'\nUang anda kurang sebesar {total - bayar}\n')
+        
+    # Update stock buah
+    stock_apel -= n_apel
+    stock_jeruk -= n_jeruk
+    stock_anggur -= n_anggur
 
+    # Konfirmasi belanja ulang
+    konfirmasi = input('Apakah akan melakukan belanja lagi?: ')
+    if konfirmasi == 'no':
+        break
    
